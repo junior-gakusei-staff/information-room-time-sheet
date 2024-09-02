946b0ce077b6d615996f656719fe58832ab6d5c1
junior-gakusei-staff <ccjunior@ccmailg.meijo-u.ac.jp>
2024-09-02 14:21:35 +0900

    build通った！

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,27 @@ import Head from 'next/head';
 import { useState, useEffect } from 'react';
 import Timetable from '@/app/components/Timetable';
 import Link from 'next/link'
-async function getData(){
+import { ApiResponse, Timetables, ClassInfo, HomeProps, TimetableProps } from '@/app/types/types';
+
+async function getData(): Promise<ApiResponse> {
   const response = await fetch("https://script.google.com/macros/s/AKfycbyiuvHKoKucQ-6TsQzxGhyZZ-rBbsMor4bAeOElB5n8VJqNO0vCNMehjpCmZSAbJehE/exec");
-  const data = await response.json();
-  console.log(data)
+  const data: ApiResponse = await response.json();
+  console.log(data);
   return data;
 }
 
-const Home = () => {
-    const [timetable, setTimetable] = useState(null);
+const Home: React.FC<HomeProps> = () => {
+    const [timetable, setTimetable] = useState<ApiResponse | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
             const data = await getData();
             setTimetable(data);
-            
         };
         fetchData();
     }, []);
 
     if (!timetable) return <div>Loading...</div>;
-
 
     return (
         <div>
@@ -38,15 +38,11 @@ const Home = () => {
                         <li><Link href="/" className="text-xl">タワー7階</Link></li>
                         <li><Link href="/" className="text-xl">E棟</Link></li>
                         <li><Link href="/" className="text-xl">申請書対応済</Link></li>
-                    </ul>
-                    
-                    
-                    
-                    
+                    </ul>   
                 </nav>
                 <div className="grid grid-cols-1 gap-4">
-                    {timetable.Ｔ602 && <Timetable timetable={timetable?.Ｔ602} roomName="T-602" />}
-                    {timetable.Ｔ603 && <Timetable timetable={timetable.Ｔ603} roomName="T-603" />}
+                    {timetable.T602 && <Timetable timetable={timetable.T602} roomName="T-602" />}
+                    {timetable.T603 && <Timetable timetable={timetable.T603} roomName="T-603" />}
                 </div>
             </main>
         </div>
