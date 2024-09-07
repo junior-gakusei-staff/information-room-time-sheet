14d9d7e16375177c9d99749d044a8d5e853e3609
junior-gakusei-staff <ccjunior@ccmailg.meijo-u.ac.jp>
2024-09-07 10:26:44 +0900

    コンポーネントを整理

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-"use client"
+
 import Head from 'next/head';
-import { useState, useEffect } from 'react';
 import Timetable from '@/app/components/Timetable';
 import Link from 'next/link'
-import { ApiResponse, Timetables, ClassInfo, HomeProps, TimetableProps } from '@/app/types/types';
+import { ApiResponse,  HomeProps } from '@/app/types/types';
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: '情報処理教室時間割表',
+  }
 
 async function getData(): Promise<ApiResponse> {
   const response = await fetch(process.env.NEXT_PUBLIC_API_URL as string);
@@ -12,42 +16,41 @@ async function getData(): Promise<ApiResponse> {
   return data;
 }
 
-const Home: React.FC<HomeProps> = () => {
-    const [timetable, setTimetable] = useState<ApiResponse | null>(null);
+const Home: React.FC<HomeProps> = async() => {
+    try{
+        const timetable = await getData()
+        return (
+            <div>
+    
+                <main className="container mx-auto p-4">
+                    <nav className="flex justify-between mb-4">
+                        <h1 className="text-3xl font-bold mb-4">情報処理教室時間割表</h1>
+                        <ul className='flex space-x-4 '>
+                            <li><Link href="/" className="text-xl border-b">タワー6階</Link></li>
+                            <li><Link href="/" className="text-xl border-b">タワー7階</Link></li>
+                            <li><Link href="/" className="text-xl border-b">E棟</Link></li>
+                            <li><Link href="/" className="text-xl border-b">申請書対応済</Link></li>
+                        </ul>   
+                    </nav>
+                    <div className="grid grid-cols-1 gap-4 ">
+                        {timetable.T602 && <Timetable timetable={timetable.T602} roomName="T-602" />}
+                        {timetable.T603 && <Timetable timetable={timetable.T603} roomName="T-603" />}
+                    </div>
+                </main>
+            </div>
+        );
+    }catch(e){
+        return(
+            <>
+            <h1>error</h1>
+            </>
+        )
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await getData();
-            setTimetable(data);
-        };
-        fetchData();
-    }, []);
+    }
+    
+    
 
-    if (!timetable) return <div>Loading...</div>;
 
-    return (
-        <div>
-            <Head>
-                <title>Timetable</title>
-            </Head>
-            
-            <main className="container mx-auto p-4">
-                <nav className="flex justify-between mb-4">
-                    <h1 className="text-3xl font-bold mb-4">情報処理教室時間割表</h1>
-                    <ul className='flex space-x-4 '>
-                        <li><Link href="/" className="text-xl border-b">タワー6階</Link></li>
-                        <li><Link href="/" className="text-xl border-b">タワー7階</Link></li>
-                        <li><Link href="/" className="text-xl border-b">E棟</Link></li>
-                        <li><Link href="/" className="text-xl border-b">申請書対応済</Link></li>
-                    </ul>   
-                </nav>
-                <div className="grid grid-cols-1 gap-4 ">
-                    {timetable.T602 && <Timetable timetable={timetable.T602} roomName="T-602" />}
-                    {timetable.T603 && <Timetable timetable={timetable.T603} roomName="T-603" />}
-                </div>
-            </main>
-        </div>
-    );
 };
 
 export default Home;
